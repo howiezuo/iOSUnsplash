ff0f69782d50d6d52b1b8cecd89551990dae939b
Howie Zuo <howie.zuo@gmail.com>
2016-10-17 08:56:14 +0900

    first commit

---
+++ iOSUnsplash/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOSUnsplash
+//
+//  Created by Zuo Hao on 2016/10/17.
+//  Copyright © 2016 Howie Zuo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
